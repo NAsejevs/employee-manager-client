57ad2a37e5f96358ae8d83e86f525251d8d615dc
Nils Asejevs <nils.asejevs@gmail.com>
2019-06-09 01:50:19 +0300

    Adjust config

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,8 +5,8 @@ export const production = true;
 export const serverURL = production
 	? "http://192.168.1.150:8080"
 	: "http://localhost:8080";
-export const employeeUpdateInterval = 60000;
-export const workLogUpdateInterval = 60000;
+export const employeeUpdateInterval = 5000;
+export const workLogUpdateInterval = 5000;
 
 export const request = axios.create({
 	baseURL: serverURL,
